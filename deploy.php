bdc88c2ab4d1b85eec5c4cbc502621b576b3ba82
Arinrare <5018715+arinrare@users.noreply.github.com>
2026-04-29 14:22:03 +1000

    Modify deploy.sh to specify a Project Euler deployment

--- deploy.php
+++ deploy.php
@@ -2,6 +2,6 @@
 // ... (Keep your signature verification logic here) ...
 
 // Create a flag file
-file_put_contents('../../.deploy_trigger', 'triggered');
+file_put_contents('../../.deploy_trigger', 'euler');
 echo "Deployment queued.";
 ?>
